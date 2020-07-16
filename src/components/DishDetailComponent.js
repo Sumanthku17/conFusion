aec22cdeb5013b76f84d999762e4cc5d066336c8
sumanth <sumanthku17@gmail.com>
2020-07-16 05:42:47 +0530

    functional component

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg,  CardBody, CardText, CardTitle } from 'reactstrap';
 import moment from 'moment';
-class DishDetail extends Component {
-    renderDish(dish) {
+
+    
+    function RenderDish({dish}) {
         if (dish!=null){
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -22,13 +23,13 @@ class DishDetail extends Component {
         }
     }
 
-    renderComments(dish) {
+    function RenderComments({dish}) {
         if(dish != null) {
             return (
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                         {dish.comments.map(comment => (
-                            <ul className="list-unstyled">
+                            <ul className="list-unstyled" key={comment.id}>
                                 <li>
                                     <p>{comment.comment}</p>
                                     <p>-- {comment.author} , {moment(comment.date).format('MMM DD, YYYY')}</p>
@@ -39,19 +40,22 @@ class DishDetail extends Component {
                 </div>);
         } else {
             return(
+                console.log("hello"),
                 <div></div>
             );
         }
     }
-    render() {
+    const DishDetail =(props) =>{
+
+        console.log('dishDetail Component render is invoked!')
         return(
             <div className="container">
                <div className="row">
-                {this.renderDish(this.props.dish)}
-                {this.renderComments(this.props.dish)} 
+                <RenderDish dish={props.dish}/>
+                <RenderComments dish={props.dish}/>
                 </div>
             </div>
         );
     }
-}
+
 export default DishDetail;
